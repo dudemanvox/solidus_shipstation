b3e20f597c92abcc576a3f3b53225f265243e224
Tommy Bregar <tommybregar@gmail.com>
2021-02-16 21:32:30 -0700

    Don't force_ssl on controller if Rails 6.1+
    * force_ssl on controllers removed in 6.1
    * if rails version is greater than 6, do not force. recycle the deprecation warning shown in 6.0-stable

--- lib/spree/basic_ssl_authentication.rb
+++ lib/spree/basic_ssl_authentication.rb
@@ -5,7 +5,17 @@ module Spree
     extend ActiveSupport::Concern
 
     included do
-      force_ssl if: :ssl_configured?
+      if !(Rails.version[/(^\d\.\d)/].to_f > 6)
+        force_ssl if: :ssl_configured?
+      else
+        ActiveSupport::Deprecation.warn(<<-MESSAGE.squish)
+          Controller-level `force_ssl` has been deprecated and removed from Rails 6.1. 
+          Please enable `config.force_ssl` in your environment configuration to enable 
+          the ActionDispatch::SSL middleware to more fully enforce that your application 
+          communicate over HTTPS. If needed, you can use `config.ssl_options` to exempt 
+          matching endpoints from being redirected to HTTPS.
+          MESSAGE
+      end
       before_action :authenticate
     end
 
